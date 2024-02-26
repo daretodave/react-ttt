825b04c1ddc1bfd4a944ac89ebeb13ee5b908ee7
David Rehmat <me@dave.blue>
2024-02-26 11:18:39 -0500

    player info elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import PlayerInfo from "./components/PlayerInfo.jsx";
 
 function App() {
-  
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+
+    return (<main>
+            <div id="game-container">
+                <ol id="players">
+                    <PlayerInfo name="Player 1" symbol="X"/>
+                    <PlayerInfo name="Player 2" symbol="O"/>
+                </ol>
+                Game Board
+
+            </div>
+        </main>
+
+    )
 }
 
 export default App
---
+++ src/components/PlayerInfo.jsx
@@ -0,0 +1,9 @@
+export default function PlayerInfo({name, symbol}) {
+    return (<li>
+    <span className="player">
+            <span className="player-name">{name}</span>
+            <span className="player-symbol">{symbol}</span>
+        </span>
+        <button>Edit</button>
+    </li>)
+}
