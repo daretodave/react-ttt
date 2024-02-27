e236bf54f9e5f77afc30353a7e60a67604fc073b
David Rehmat <me@dave.blue>
2024-02-27 11:54:53 -0500

    win condition

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import PlayerInfo from "./components/PlayerInfo.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import {useState} from "react";
 import Log from "./components/Log.jsx";
-
+import {WINNING_COMBINATIONS} from "./winning-combinations.js";
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
 function getActivePlayer(turns) {
     let activePlayer = 'X';
     if (turns.length && turns[0].player === 'X') {
@@ -15,6 +20,31 @@ function getActivePlayer(turns) {
 function App() {
     const [gameTurns, setGameTurns] = useState([]);
     const activePlayer = getActivePlayer(gameTurns);
+
+    const gameBoard = [...initialGameBoard.map(row => [...row])];
+
+    for (const {tile, player} of gameTurns) {
+        const {
+            row,
+            col
+        } = tile;
+
+        gameBoard[row][col] = player;
+    }
+
+    let winner = null;
+
+    for (let [comboA, comboB, comboC] of WINNING_COMBINATIONS) {
+        const firstTile = gameBoard[comboA.row][comboA.column];
+        const secondTile = gameBoard[comboB.row][comboB.column];
+        const thirdTile = gameBoard[comboC.row][comboC.column];
+
+        if (firstTile
+            && firstTile === secondTile
+            && firstTile === thirdTile) {
+            winner = firstTile;
+        }
+    }
 
     function handleSelectTile(rowIndex, columnIndex) {
         setGameTurns(prevTurns => [{
@@ -32,7 +62,8 @@ function App() {
                     <PlayerInfo isActive={activePlayer === 'X'} name="Player 1" symbol="X"/>
                     <PlayerInfo isActive={activePlayer === 'O'} name="Player 2" symbol="O"/>
                 </ol>
-                <GameBoard turns={gameTurns} onSelectTile={handleSelectTile}/>
+                {winner && <p>You won, {winner}!</p>}
+                <GameBoard gameBoard={gameBoard} turns={gameTurns} onSelectTile={handleSelectTile}/>
             </div>
             <Log turns={gameTurns}/>
         </main>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,39 +1,5 @@
-import {useState} from "react";
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
-
-export default function GameBoard({onSelectTile, turns}) {
-    let gameBoard = [...initialGameBoard.map(row => [...row])];
-
-    for (const {tile, player} of turns) {
-        const {
-            row,
-            col
-        } = tile;
-
-        gameBoard[row][col] = player;
-    }
-    // const [
-    //     gameBoard,
-    //     setGameBoard,
-    // ] = useState(initialGameBoard);
-    //
-    // function handleSelectTile(rowIndex, columnIndex) {
-    //     setGameBoard(priorGameBoard => {
-    //         const board = [...priorGameBoard.map(row => [...row])]
-    //
-    //         board[rowIndex][columnIndex] = activePlayerSymbol;
-    //
-    //         return board;
-    //     });
-    //
-    //     onSelectTile();
-    // }
-
+export default function GameBoard({onSelectTile, gameBoard}) {
     return (
         <ol id="game-board">
             {gameBoard.map((row, rowIndex) => {
@@ -41,7 +7,7 @@ export default function GameBoard({onSelectTile, turns}) {
                     <ol>{
                         row.map((symbol, columnIndex) =>
                             <li key={columnIndex}>
-                                <button onClick={() => onSelectTile(rowIndex, columnIndex)}>{symbol}</button>
+                                <button disabled={symbol !== null} onClick={() => onSelectTile(rowIndex, columnIndex)}>{symbol}</button>
                             </li>
                         )
                     }</ol>
