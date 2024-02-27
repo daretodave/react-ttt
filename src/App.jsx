723f78b5fa4ee2d28b46c00f4f0e4c69278d34a9
David Rehmat <me@dave.blue>
2024-02-27 10:23:19 -0500

    render grind

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import PlayerInfo from "./components/PlayerInfo.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
                     <PlayerInfo name="Player 1" symbol="X"/>
                     <PlayerInfo name="Player 2" symbol="O"/>
                 </ol>
-                Game Board
+                <GameBoard />
 
             </div>
         </main>
